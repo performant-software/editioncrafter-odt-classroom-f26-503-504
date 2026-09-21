738292fdad725adbc53f06101e79d9cdf35e1d30
Rebecca Black <rebecca.ann.black@gmail.com>
2026-09-21 09:46:44 -0400

    data 9-21-26

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  base: "/editioncrafter-odt-classroom-f26-501-502",
+  base: "/editioncrafter-odt-classroom-f26-503-504",
 });
